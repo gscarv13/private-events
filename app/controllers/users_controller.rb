482ba0a33ccf16c1defbab12903e199de821fb6e
Gustavo Carvalho <gsc13@tuta.io>
2021-03-04 18:15:08 -0300

    Add user actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,29 @@
 class UsersController < ApplicationController
+  
+  def index    
+  end
+
   def new
     @user = User.new
   end
 
   def create
-    @user = User.create(user_params)
+    @user = User.new(user_params)
+    
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to '/users/index'
+    else
+      render :new
+    end
   end
 
   def show
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username)
   end
 end
