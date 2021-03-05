738f56a851bc023855439642eb17a78ae31a9eee
Gustavo Carvalho <gsc13@tuta.io>
2021-03-05 14:50:26 -0300

    Add relation between User and Event

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,3 @@
 class User < ApplicationRecord
+  has_many :events, class_name: 'Event', foreign_key: 'creator' # This tells rails that creator on events is associated with the user_id
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_04_214119) do
+ActiveRecord::Schema.define(version: 2021_03_04_221121) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2021_03_04_214119) do
     t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "creator"
   end
 
   create_table "users", force: :cascade do |t|
