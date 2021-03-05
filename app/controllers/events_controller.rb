a20a8ba15d186e3e9533a1e0a43e58fc3bdd8fa5
Gustavo Carvalho <gsc13@tuta.io>
2021-03-05 18:30:49 -0300

    Add Event actions to controller, create Event views

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,14 +1,30 @@
 class EventsController < ApplicationController
   def index
+    @events = Event.all
   end
 
   def new
+    @event = Event.new
   end
 
   def create
+    @event = current_user.events.build(event_params)
+
+    if @event.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def show
+    @event = Event.find(params[:id])
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :location, :date, :description)
   end
 
 end
