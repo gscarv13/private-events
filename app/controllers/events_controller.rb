cc867b8351ffecd3ca1ddb33871248f64e0eef7e
Gustavo Carvalho <gsc13@tuta.io>
2021-03-09 18:29:12 -0300

    Add future and past events to index and User show page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,7 @@
 class EventsController < ApplicationController
   def index
-    @events = Event.all
+    @future = Event.upcomming_events
+    @past = Event.past_events
   end
 
   def new
--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,7 @@ class Event < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: 'creator'
   has_many :attendances, class_name: 'Attendance', foreign_key: 'attended_event'
   has_many :attendees, through: :attendance, source: 'attendee'
+
+  scope :upcomming_events, -> { where('date > ?', Time.now) }
+  scope :past_events, -> { where('date < ?', Time.now) }
 end
