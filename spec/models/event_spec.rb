6739c9fb54247876f45f8626522c2e73b4ec781a
Gustavo Carvalho <gsc13@tuta.io>
2021-03-10 19:11:33 -0300

    Add event and user unit testing

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before(:all) do
+    params = { name: 'event', date: Time.now, location: 'Online', description: 'description of the event' }
+    @event = Event.new(params)
+    @no_name = Event.new(params.merge(name: nil)).save
+    @no_description = Event.new(params.merge(description: nil)).save
+    @no_date = Event.new(params.merge(date: nil)).save
+    @no_location = Event.new(params.merge(location: nil)).save
+  end
+
+  context 'validation tests' do
+    it 'ensure title presence' do
+      expect(@no_name).to be false
+    end
+
+    it 'ensure description presence' do
+      expect(@no_description).to be false
+    end
+
+    it 'ensure date presence' do
+      expect(@no_date).to be false
+    end
+
+    it 'ensure location presence' do
+      expect(@no_location).to be false
+    end
+  end
 end
