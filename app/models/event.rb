12a564b048571d64c3a690c05da08b8b871d82af
Gustavo Carvalho <gsc13@tuta.io>
2021-03-10 16:48:30 -0300

    Fix typo

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@ class Event < ApplicationRecord
   validates :name, presence: true, length: { minimum: 4, maximum: 45 }
   validates :date, presence: true
   validates :location, presence: true, length: { minimum: 4 }
-  validates :description, preasence: true, length: { minimum: 6 }
+  validates :description, presence: true, length: { minimum: 6 }
 
   belongs_to :user, class_name: 'User', foreign_key: 'creator'
   has_many :attendances, class_name: 'Attendance', foreign_key: 'attended_event'
