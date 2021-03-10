2f8d5b00fa494046eefd7e7f7bb9ae4218d6b5eb
Gustavo Carvalho <gsc13@tuta.io>
2021-03-09 22:06:55 -0300

    Add validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,9 @@
 class Event < ApplicationRecord
+  validates :name, presence: true, length: { minimum: 4, maximum: 45 }
+  validates :date, presence: true
+  validates :location, presence: true, length: { minimum: 4 }
+  validates :description, preasence: true, length: { minimum: 6 }
+
   belongs_to :user, class_name: 'User', foreign_key: 'creator'
   has_many :attendances, class_name: 'Attendance', foreign_key: 'attended_event'
   has_many :attendees, through: :attendance, source: 'attendee'
