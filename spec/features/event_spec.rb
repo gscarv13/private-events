03cf7b54fc4b81ef2ef1cdca7b6227c822d84272
Gustavo Carvalho <gsc13@tuta.io>
2021-03-11 17:46:11 -0300

    Correct integration test

--- spec/features/event_spec.rb
+++ spec/features/event_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.feature "Events", type: :feature do 
-  
   context 'create new event' do
     before(:each) do
       visit new_user_path
@@ -21,7 +20,7 @@ RSpec.feature "Events", type: :feature do
       expect(page).to have_content('Test Event')
     end
 
-    scenario 'should not be successful without date, and preserve correct fields' do
+    scenario 'should not be successful if missing data' do
       visit new_event_path
       within('form') do
         fill_in 'Name', with: 'Test Event'
@@ -30,7 +29,17 @@ RSpec.feature "Events", type: :feature do
       end
       click_on('Create Event')
       expect(page).to have_content('can\'t be blank')
-      expect(page).to have_content('Test Event')
+    end
+
+    scenario 'should not be successful and preserve correct fields' do
+      visit new_event_path
+      within('form') do
+        fill_in 'Name', with: 'Test Event'
+        fill_in 'Location', with: 'London, UK'
+        fill_in 'Description', with: 'This is a test event'
+      end
+      click_on('Create Event')
+      expect(find_field('Name').value).to have_content('Test Event')
     end
   end
 end
