1268c3983fc67c678c2639d0b632d6dd7ee4132b
Gustavo Carvalho <gsc13@tuta.io>
2021-03-12 17:00:57 -0300

    Add attend event functionality

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -18,6 +18,27 @@ class EventsController < ApplicationController
     end
   end
 
+  def participate
+    @event = Event.find(params[:id])
+
+    if @event.attendees.include?(current_user)
+      flash[:alert] = 'You are already participating'
+    else
+      @attendance = Attendance.create(attendee: current_user, attended_event: @event)
+      flash[:notice] = 'Added successfully'
+    end
+
+    redirect_to @event
+  end
+
+  def cancel_participation
+    @event = Event.find(params[:id])
+    @event.attendees.delete(current_user)
+    flash[:alert] = 'You are no longer on the attendance list'
+
+    redirect_to @event
+  end
+
   def show
     @event = Event.find(params[:id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   resources :users, only: %i[new create show]
   resources :events, only: %i[new create show]
 
+  get 'participate/:id', to: 'events#participate', as: 'participate'
+  delete 'participate/:id', to: 'events#cancel_participation', as: 'destroy_participation'
+
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create', as: 'new_session'
   delete 'login', to: 'sessions#destroy', as: 'destroy_session'
