88e34982fe04e2dea481733881643d6cde5a5da2
Gustavo Carvalho <gsc13@tuta.io>
2021-03-12 12:46:55 -0300

    Fix through association, Make scope global

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,8 +6,5 @@ class Event < ApplicationRecord
 
   belongs_to :user, class_name: 'User', foreign_key: 'creator'
   has_many :attendances, class_name: 'Attendance', foreign_key: 'attended_event'
-  has_many :attendees, through: :attendance, source: 'attendee'
-
-  scope :upcomming_events, -> { where('date > ?', Time.now) }
-  scope :past_events, -> { where('date < ?', Time.now) }
+  has_many :attendees, through: :attendances, source: 'attendee'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
 
   has_many :events, class_name: 'Event', foreign_key: 'creator'
   has_many :attendances, class_name: 'Attendance', foreign_key: 'attendee'
-  has_many :attended_events, through: :attendance, source: 'attended_event'
+  has_many :attended_events, through: :attendances, source: 'attended_event'
 end
