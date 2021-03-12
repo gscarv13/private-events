b174fb9033df787954ac15fd2594ff0abc0c8234
Gustavo Carvalho <gsc13@tuta.io>
2021-03-12 18:36:32 -0300

    Add integration test to attendance functionality

--- spec/features/event_spec.rb
+++ spec/features/event_spec.rb
@@ -2,13 +2,13 @@
 require 'rails_helper'
 
 RSpec.feature 'Events', type: :feature do
-  context 'create new event' do
-    before(:each) do
-      visit new_user_path
-      within('form') { fill_in 'Username', with: 'NewUser' }
-      click_on('Create User')
-    end
+  before(:each) do
+    visit new_user_path
+    within('form') { fill_in 'Username', with: 'NewUser' }
+    click_on('Create User')
+  end
 
+  context 'create new event' do
     scenario 'should be successful' do
       visit new_event_path
       within('form') do
@@ -41,6 +41,33 @@ RSpec.feature 'Events', type: :feature do
       end
       click_on('Create Event')
       expect(find_field('Name').value).to have_content('Test Event')
+    end
+  end
+
+  context 'attend events' do
+    before(:each) do
+      visit new_event_path
+      within('form') do
+        fill_in 'Name', with: 'Test Event'
+        fill_in 'Location', with: 'London, UK'
+        fill_in 'Date', with: Time.new.next_month
+        fill_in 'Description', with: 'This is a test event'
+      end
+      click_on('Create Event')
+    end
+    scenario 'should be able to opt in to participate' do
+      visit root_path
+      click_on('Details')
+      click_on('Participate')
+      expect(page.find('.attendants')).to have_content('NewUser')
+    end
+
+    scenario 'should be able to cancel participation' do
+      visit root_path
+      click_on('Details')
+      click_on('Participate')
+      click_on('Cancel Participation')
+      expect(page.find('.attendants')).to_not have_content('NewUser')
     end
   end
 end
